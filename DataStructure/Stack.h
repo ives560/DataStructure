6d1ab4bf38ddd53dd1184281dead3a03b11928b3
ives560 <kingwan560@163.com>
2017-11-17 15:17:14 +0800

    堆栈模板和添加ZoVector

--- DataStructure/Stack.h
+++ DataStructure/Stack.h
@@ -2,20 +2,15 @@
 
 #include"Comm.h"
 
-//线性栈
-typedef struct
-{
-	ElemType data[MAX];
-	int top;
-}SqStack;
+ZO_BEGIN_MAMESPACE
 
-//两栈共享空间结构
-typedef struct
+//线性栈
+template <typename T>
+struct SqStack
 {
-	ElemType data[MAX];
-	int top1;
-	int top2;
-}SqDoubleStack;
+	T data[MAX];
+	int top;
+};
 
 
 //链栈节点
@@ -25,41 +20,46 @@ typedef struct LNode
 	LNode *next;
 }LNode;
 
+template <typename T>
 class Stack
 {
+
+private:
+	SqStack S;
+
 public:
 	Stack();
 	~Stack();
 	// 进栈
-	Status Push(SqStack& S, ElemType e);
+	Status Push(T e);
 	// 出栈
-	Status Pop(SqStack& S, ElemType& e);
-	Status Push(SqDoubleStack& S, ElemType e, int stackNumber);
-	// 出栈
-	Status Pop(SqDoubleStack& S, ElemType& e, int stackNumber);
+	Status Pop(T& e);
+
 	// 入栈
-	Status Push(LNode& S, ElemType e);
+	Status Push(LNode& S, T e);
 	// 出栈
-	Status Pop(LNode& S, ElemType& e);
+	Status Pop(LNode& S, T& e);
 	// 是否为空栈
 	int StackEmpty(LNode& S);
 	// 链栈初始化
 	void InitStack(LNode* S);
 };
 
-
-Stack::Stack()
+template <typename T>
+Stack<T>::Stack()
 {
+	S.top = -1;
 }
 
-
-Stack::~Stack()
+template <typename T>
+Stack<T>::~Stack()
 {
 }
 
 
 // 进栈
-Status Stack::Push(SqStack& S, ElemType e)
+template <typename T>
+Status Stack<T>::Push(T e)
 {
 	if (S.top == MAX - 1)
 	{
@@ -74,7 +74,8 @@ Status Stack::Push(SqStack& S, ElemType e)
 
 
 // 出栈
-Status Stack::Pop(SqStack& S, ElemType& e)
+template <typename T>
+Status Stack<T>::Pop(T& e)
 {
 	if (S.top == -1)
 		return ERROR;
@@ -85,44 +86,10 @@ Status Stack::Pop(SqStack& S, ElemType& e)
 	return OK;
 }
 
-/*----------------------两栈共享空间结构------------------------------*/
-// 进栈
-Status Stack::Push(SqDoubleStack& S, ElemType e, int stackNumber)
-{
-	if (S.top1 + 1 == S.top2)//栈满
-		return ERROR;
-
-	if (stackNumber == 1)
-		S.data[++S.top1] = e;
-	else if (stackNumber == 2)
-		S.data[--S.top2] = e;
-
-
-	return OK;
-}
-
-
-// 出栈
-Status Stack::Pop(SqDoubleStack& S, ElemType& e, int stackNumber)
-{
-	if (stackNumber == 1)
-	{
-		if (S.top1 == -1)//栈1空栈
-			return ERROR;
-		e = S.data[S.top1--];
-	}
-	else if (stackNumber == 2)//
-	{
-		if (S.top2 == MAX)//栈2空栈
-			return ERROR;
-		e = S.data[S.top2++];
-	}
-	return OK;
-}
-
 /*-------------------------链栈--------------------------------*/
 // 入栈，从链栈头入栈
-Status Stack::Push(LNode& S, ElemType e)
+template <typename T>
+Status Stack<T>::Push(LNode& S, ElemType e)
 {
 	LNode* s = (LNode*)malloc(sizeof(LNode));
 	s->next = NULL;
@@ -136,7 +103,8 @@ Status Stack::Push(LNode& S, ElemType e)
 
 
 // 出栈，从链栈头出栈
-Status Stack::Pop(LNode& S, ElemType& e)
+template <typename T>
+Status Stack<T>::Pop(LNode& S, ElemType& e)
 {
 	LNode* p;
 	if (S.next == NULL)//栈空
@@ -152,7 +120,8 @@ Status Stack::Pop(LNode& S, ElemType& e)
 
 
 // 是否为空栈,空返回TRUE
-int Stack::StackEmpty(LNode& S)
+template <typename T>
+int Stack<T>::StackEmpty(LNode& S)
 {
 	if (S.next == NULL)
 	{
@@ -164,8 +133,11 @@ int Stack::StackEmpty(LNode& S)
 
 
 // 链栈初始化
-void Stack::InitStack(LNode* S)
+template <typename T>
+void Stack<T>::InitStack(LNode* S)
 {
 	S = (LNode*)malloc(sizeof(LNode));
 	S->next = NULL;
 }
+
+ZO_END_NAMESPACE
---
+++ DataStructure/ZoVector.h
@@ -0,0 +1,8 @@
+#pragma once
+class ZoVector
+{
+public:
+	ZoVector();
+	~ZoVector();
+};
+
